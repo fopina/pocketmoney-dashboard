ba9713759d4d05579ed6e590915e5777def5605b
Filipe Pina <hzlu1ot0@duck.com>
2025-04-23 01:11:16 +0100

    x

--- push.py
+++ push.py
@@ -71,6 +71,7 @@ class Push:
             doc_id = trans['ID']
             del trans['ID']
 
+            trans['amount'] = float(trans['amount'])
             trans['transaction'] = self.transactions[trans['transaction']]
             if trans['category']:
                 trans['category'] = self.categories[trans['category']]
@@ -93,10 +94,15 @@ class Push:
                 print('Errors:', json.dumps(failed, indent=2))
 
     def setup(self):
+        r = self.osd_client.delete_index_pattern(self.index)
+        if r.status_code != 404:
+            r.raise_for_status()
         r = self.osd_client.create_index_pattern(self.index, 'transaction.date')
         r.raise_for_status()
 
     def __call__(self):
+        r = self.osd_client.import_object(Path('dashboard.ndjson'), overwrite=True)
+        print(r.content)
         self.setup()
         self.push_to_os()
 
@@ -105,6 +111,7 @@ class OSDClient(requests.Session):
     def __init__(self, host, port):
         super().__init__()
         self.base_url = f'http://{host}:{port}'
+        self.headers['osd-xsrf'] = 'true'
 
     def request(self, method, url, **kwargs):
         return super().request(method, f'{self.base_url}/{url}', **kwargs)
@@ -116,6 +123,25 @@ class OSDClient(requests.Session):
             json={'attributes': {'title': name, 'timeFieldName': time_field}},
             headers={'osd-xsrf': 'true'},
         )
+
+    def delete_index_pattern(self, name):
+        return self.delete(
+            f'api/saved_objects/index-pattern/{name}',
+        )
+
+    def export_dashboard(self, name):
+        return self.post(
+            'api/saved_objects/_export',
+            json={'objects': [{'id': name, 'type': 'dashboard'}], 'includeReferencesDeep': True},
+        )
+
+    def import_object(self, file: Path, create_new_copies=False, overwrite=False):
+        with file.open('r') as f:
+            return self.post(
+                'api/saved_objects/_import',
+                params={'createNewCopies': create_new_copies, 'overwrite': overwrite},
+                files={'file': f},
+            )
 
 
 if __name__ == '__main__':
