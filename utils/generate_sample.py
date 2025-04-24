551322b3fd38f3ef7d2e7402a90ddc75287ee550
Filipe Pina <hzlu1ot0@duck.com>
2025-04-24 01:53:18 +0100

    x

---
+++ utils/generate_sample.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+from pathlib import Path
+
+import click
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+from functools import cached_property
+
+import classyclick
+
+from push import OSDClient
+
+
+@classyclick.command()
+class GenerateSample:
+    """Generate a random sample of transactions to test the dashboards and make screenshots"""
+
+    output: Path = classyclick.option(
+        default=Path(__file__).parent.parent / 'samples' / 'sample_db_dump.json',
+        help='Where to save the random sample',
+        show_default=True,
+    )
+
+    def __call__(self):
+        self.output.write_text(json.dumps('asd'))
+
+
+if __name__ == '__main__':
+    GenerateSample()
